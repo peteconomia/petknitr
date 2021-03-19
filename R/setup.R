fa4ff7356395f3a11773a5e78ddd942bf4ce8756
pedrocastroo <pdesacastro@gmail.com>
2021-03-18 22:30:46 -0300

    initial

---
+++ R/setup.R
@@ -0,0 +1,39 @@
+#' @title knitr setup
+#' @import knitr
+#' @export 
+
+setup <- function(echo = FALSE, eval = TRUE, message = FALSE, warning = FALSE, 
+                  dev = "cairo_pdf", cache = TRUE, cache.path = "_cache/") {
+
+  knitr::opts_chunk$set(
+    echo = echo,
+    eval = eval,
+    message = message,
+    warning = warning,
+    dev = dev,
+    cache = cache,
+    cache.path = cache.path
+  )
+
+  knitr::knit_hooks$set(
+    plot = function(x, options) {
+      paste0(
+        if ((options$wrap == "open" && !is.null(options$wrap)) || !is.null(options$wrap) && options$wrap == T) "\\Begin{wrapfig}\n",
+        "\\Begin{fig}\n",
+        "\\caption{", options$fig.cap, "}",
+        "\\label{fig:", options$label, "}\n",
+        "\\subcap{", options$fig.subcap, "}",
+        "\\includegraphics{", x, "}",
+        if (!is.null(options$fig.source)) {
+          paste0("\n\\source{", options$fig.source, "}")
+        },
+        if (!is.null(options$fig.notes)) {
+          paste0("\\notes{", options$fig.notes, "}")
+        },
+        "\\End{fig}",
+        if ((options$wrap == "close" && !is.null(options$wrap)) || !is.null(options$wrap) && options$wrap == T) "\\End{wrapfig}"
+      )
+    }
+  )
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# petknitr
+
+`knitr` setup template
+
+### Instalação
+
+```r
+# install.packages("devtools")
+devtools::install_github("peteconomia/petknitr")
+```
+### Uso
+
+```r
+library(petknitr)
+petknitr::setup()
+```
