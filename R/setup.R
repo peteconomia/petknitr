5fc926db68d4be1dacc74059a679b81b072e04cb
pedrocastroo <pdesacastro@gmail.com>
2021-05-08 01:13:47 -0300

    fix

--- R/setup.R
+++ R/setup.R
@@ -27,9 +27,9 @@ setup <- function(echo = FALSE,
       knitr::raw_latex(
         paste0(
           if ((options$wrap == "open" && !is.null(options$wrap)) || !is.null(options$wrap) && options$wrap == T) {
-            "\\begin{figure}\n"
+            "\\begin{figure}[!h]\n"
           },
-          sprintf("\\begin{subfigure}\n\\caption{%s}\\label{fig:%s}\n\\subcap{%s}\n\\includegraphics{%s}\n\\source{%s}\n", options$fig.cap, options$label, options$fig.subcap, x, options$fig.source),
+          sprintf("\\begin{subfigure}{\\linewidth}\n\\caption{%s\\label{fig:%s}}\n\\subcap{%s}\n\\includegraphics{%s}\n\\source{%s}\n", options$fig.cap, options$label, options$fig.subcap, x, options$fig.source),
           if (!is.null(options$fig.notes)) {
             sprintf("\\notes{%s}\n", options$fig.notes)
           },
